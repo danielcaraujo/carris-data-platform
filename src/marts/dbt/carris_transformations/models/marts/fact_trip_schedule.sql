2b89acb65871c5c4b28dd6f1e3dea08088235984
MargaridaGomes <margaridamfgomes@gmail.com>
2025-07-23 12:00:36 +0100

    dbt: initial fact table and configs

---
+++ src/marts/dbt/carris_transformations/models/marts/fact_trip_schedule.sql
@@ -0,0 +1,28 @@
+{% set surrogate_key_columns = [
+    "t.trip_id",
+    "t.route_id"
+] %}
+
+with
+    trips as (
+        select *
+        FROM {{ source ('carris_transformations','staging_trips') }}
+    ),
+
+    final as (
+        select
+            {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
+            as trip_key,
+            t.trip_id,
+            t.route_id,
+            t.pattern_id,
+            t.shape_id,
+            t.service_id,
+            t.direction_id,           
+            t.trip_headsign,
+            current_timestamp as ingested_at
+        from trips t
+    )
+
+select *
+from final
