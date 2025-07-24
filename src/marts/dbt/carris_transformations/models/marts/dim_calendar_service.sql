45b752c3443015b48b1695ac21f4d16df06bff7a
MargaridaGomes <margaridamfgomes@gmail.com>
2025-07-24 23:55:22 +0100

    feat: added staging tables on source.yml, dim_calendar, etc

---
+++ src/marts/dbt/carris_transformations/models/marts/dim_calendar_service.sql
@@ -0,0 +1,28 @@
+{% set surrogate_key_columns = [
+    "d.date",
+    "ci.service_id"
+] %}
+
+with
+    dates as (
+        select *
+        FROM {{ source ('carris_transformations','staging_dates') }}
+    ),
+    calendar_info as (
+        select *
+        FROM {{ source ('carris_transformations','staging_calendar_dates') }}
+    ),
+    final as (
+        select
+            {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
+            as calendar_service_key,
+            d.date,
+            cd.service_id,
+            current_timestamp as ingested_at
+        from dates d 
+        left join calendar_info ci
+        on d.date = ci.date
+    )
+
+select *
+from final
