6553f3a6372c87f41af76db5818545d5a2a63441
MargaridaGomes <margaridamfgomes@gmail.com>
2025-07-25 18:09:31 +0100

    feat: added left join calendar_dates

--- src/marts/dbt/carris_transformations/models/marts/dim_calendar_service.sql
+++ src/marts/dbt/carris_transformations/models/marts/dim_calendar_service.sql
@@ -13,14 +13,14 @@ with
         FROM {{ source ('carris_transformations','staging_calendar_dates') }}
     ),
     final as (
-        select
+        select distinct
             {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
             as calendar_service_key,
             d.date,
-            cd.service_id,
+            ci.service_id,
             current_timestamp as ingested_at
         from dates d 
-        left join calendar_info ci
+        join calendar_info ci
         on d.date = ci.date
     )
 
