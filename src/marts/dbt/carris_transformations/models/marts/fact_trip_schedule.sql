ea0cbfbd83f845fcfcce31f7b2372991fce570a4
MargaridaGomes <margaridamfgomes@gmail.com>
2025-07-25 10:45:30 +0100

    feat: altered fct_trip

--- src/marts/dbt/carris_transformations/models/marts/fact_trip_schedule.sql
+++ src/marts/dbt/carris_transformations/models/marts/fact_trip_schedule.sql
@@ -8,10 +8,16 @@ with
         FROM {{ source ('carris_transformations','staging_trips') }}
     ),
 
+    calendar_service as (
+        select *
+        FROM {{ source ('carris_transformations','staging_calendar_dates') }}
+    ),
+
     final as (
         select
             {{ dbt_utils.generate_surrogate_key(surrogate_key_columns) }}
             as trip_key,
+            cs.date AS trip_date,
             t.trip_id,
             t.route_id,
             t.pattern_id,
@@ -21,6 +27,8 @@ with
             t.trip_headsign,
             current_timestamp as ingested_at
         from trips t
+        left join calendar_service cs
+        on t.service_id = cs.service_id
     )
 
 select *
